358fcf3c1333720279fcfb58f03d92b5551f1253
HE-B-CKA3KE <bhop008800@gmail.com>
2024-05-16 15:53:49 +0300

    update config files

---
+++ palindrom.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+using namespace std;
+
+bool check_polindrom(string word)
+{
+	int len = word.length();
+	for (int i = 0; i < len / 2; ++i)
+	{
+		if (word[i] != word[len - i - 1])
+		{
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	SetConsoleOutputCP(1251);
+	string str;
+	cout << "Введіть слово: ";
+	cin >> str;
+	if (check_polindrom(str))
+	{
+		cout << "Слово є поліндромом.";
+	}
+	else
+	{
+		cout << "Слово не є поліндромом"<<endl;
+	}
+	return 0;
+}
